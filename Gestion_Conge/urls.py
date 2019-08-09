e3dfeff1895fe92c617c345a0a74406d6e054bd5
Meihern <43423207+Meihern@users.noreply.github.com>
2019-08-09 11:59:43 +0100

    Added redirection to ConsultationDemandeConge from base template

--- Gestion_Conge/urls.py
+++ Gestion_Conge/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url('^demande_conge$', login_required(views.DemandeCongeView.as_view()), name='demande_conge'),
     url('^accept_demande_conge$', login_required(views.accept_demande_conge), name='accept_demande_conge'),
     url('^refuser_demande_conge$', login_required(views.refuser_demande_conge), name='refuser_demande_conge'),
-    url('consultation_demande_conge', login_required(views.ConsultationDemandeConges.as_view()), name='consultation_demande_conger')
+    url('consultation_demande_conge', login_required(views.ConsultationDemandeConges.as_view()), name='consultation_demande_conge')
 ]
